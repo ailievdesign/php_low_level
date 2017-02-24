a009c2136fb277d29a3b4ef64a9798edb72f7a53
ailievdesign <ailievdesign@gmail.com>
2017-02-24 14:56:11 +0200

    expire vfu

---
+++ expire/expire.php
@@ -0,0 +1,91 @@
+<?php
+// VFU expire content
+
+  // $now = date('Y-m-d H:i:s');
+  // $set = '2017-02-02 14:24:00';
+  // $set2 = '2018-02-02 14:24:00';
+
+$arr = array("a"=>"2012-02-02 14:24:00", "b"=>"2019-02-01 14:24:00");
+
+function expireContent ($arr, $key)
+{
+	$now = strtotime(date('Y-m-d H:i:s'));
+	$set = strtotime($arr[$key]);
+
+	if ($now > $set)
+	{
+		echo "display: block;";
+	}
+	else 
+	{
+		echo "display: none;";
+	}
+}
+
+echo expireContent($arr, "b") . "<br />";
+
+// expired arrays admin view
+
+
+
+/*
+function expireAdmin ($arr, $key)
+{
+	$now = strtotime(date('Y-m-d H:i:s'));
+	$set = strtotime($arr[$key]);
+
+	if ($now > $set)
+	{
+		
+	
+
+	}
+}
+*/
+
+
+
+function printre ( $object , $name = '' ) {
+
+    print ( '\'' . $name . '\' : ' ) ;
+
+    if ( is_array ( $object ) ) {
+        print ( '<pre>' )  ;
+        print_r ( $object ) ; 
+        print ( '</pre>' ) ;
+    } else {
+        var_dump ( $object ) ;
+    }
+
+}
+
+
+
+?>
+
+<html>
+<head>
+<title>Expire</title>
+</head>
+<body>
+
+
+<div style="<?php echo expireContent($arr, "a"); ?>">dasdas</div>
+
+<br />
+
+<pre>
+<?php
+uksort($arr, 'strcasecmp');
+echo "<pre>" . print_r($arr) ."</pre>";
+?>
+</pre>
+
+<?php
+
+echo implode(" >> ",$arr)."<br>";
+
+
+?>
+</body>
+</html>
